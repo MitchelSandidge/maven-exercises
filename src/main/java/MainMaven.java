1cd40f5055ade67e71edb72dbd592cbfee8fc23d
Mitchel Sandidge <masandidge12@gmail.com>
2020-04-08 15:51:25 -0500

    Modified the .isNumeric function

--- src/main/java/MainMaven.java
+++ src/main/java/MainMaven.java
@@ -4,18 +4,28 @@ import java.util.Scanner;
 
 public class MainMaven {
 
+//    private String isNumeric() {
+//        if ()
+//    }
+
     public static void main(String[] args) {
         Scanner userInput =  new Scanner(System.in);
+
 
 
         System.out.print("\n\tEnter something to modify your input: \n\t>");
         String userString = userInput.nextLine();
 
 
+
         System.out.print("\n\n\tUser Input: ");
         System.out.print("\n\t" + userString);
         System.out.print("\n\n\tModified Inputs:");
-        System.out.print("\n\t.isNumeric:  " + StringUtils.isNumeric(userString));
+        if (StringUtils.isNumeric(userString)) {
+            System.out.print("\n\t.isNumeric: The input is a number");
+        } else {
+            System.out.print("\n\t.isNumeric: The input is not a string");
+        }
         System.out.print("\n\t.swapCase:  " + StringUtils.swapCase(userString));
         System.out.print("\n\t.reverse:  " + StringUtils.reverse(userString));
         System.out.print("\n\t.deleteWhiteSpace:  " + StringUtils.deleteWhitespace(userString) + "\n");
